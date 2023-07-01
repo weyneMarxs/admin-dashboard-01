02cfdee78ecab1a7a9ee3cf6e1f8f4f1370dd4f3
weyne <weyneace@gmail.com>
2023-07-01 14:05:06 -0300

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: 'JavaScript Tutorial',
+    productNumber: '85743',
+    payment: 'Due',
+    status: 'Pending'
+  },
+  {
+    productName: 'CSS Full Course',
+    productNumber: '97245',
+    payment: 'Refunded',
+    status: 'Declined'
+  },
+  {
+    productName: 'Flex-Box Tutorial',
+    productNumber: '36452',
+    payment: 'Paid',
+    status: 'Active'
+  }
+]
